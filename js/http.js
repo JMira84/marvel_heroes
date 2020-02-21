dd0ecf1277b1762e4c3846ce714d09b3a6bf4728
João Mira <joao.mira01@gmail.com>
2020-02-21 01:37:05 +0000

    created a class with a fetch method

---
+++ js/http.js
@@ -0,0 +1,22 @@
+export class Http {
+
+    static fetchData(url) {
+        return new Promise((resolve, reject) => {
+            const HTTP = new XMLHttpRequest();
+
+            HTTP.open('GET', url);
+            HTTP.onreadystatechange = () => {
+                if (HTTP.readyState == XMLHttpRequest.DONE && HTTP.status == 200) {
+                    const RESPONSE_DATA = JSON.parse(HTTP.responseText);
+
+                    resolve(RESPONSE_DATA);
+                } else if (Http.readyState == XMLHttpRequest.DONE) {
+                    reject('Aconteceu um erro');
+                }
+            }
+
+            HTTP.send();
+        });
+    }
+
+}
